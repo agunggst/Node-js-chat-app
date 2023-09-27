41043c7f3e57bbee510ff9dd941ecc1308e650ee
agunggst <rqz.agastya@gmail.com>
2023-09-27 21:06:20 +0700

    done user controller

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,14 +1,47 @@
 const { user } = require('../models')
-
+const { Op } = require("sequelize")
 class UserController {
   static async getUsers (req, res) {
-
+    try {
+      const users = await user.findAll({
+        where: {
+          username: {
+            [Op.iLike]: `%${req.query.search || ''}%`
+          }
+        },
+        order: [
+          ['username', 'ASC']
+        ]
+      })
+      res.json(users)
+    } catch (error) {
+      res.send(error)
+    }
   }
   static async gotoSwitchUser (req, res) {
-
+    try {
+      const users = await user.findAll()
+      res.json(users)
+    } catch (error) {
+      res.send(error)
+    }
   }
-  static async switchLogin (req, res) {
-
+  static async switchUser (req, res) {
+    try {
+      await user.update({ isLogin: false }, {
+        where: {
+          isLogin: true
+        }
+      })
+      await user.update({ isLogin: true }, {
+        where: {
+          id: req.params.id
+        }
+      })
+      res.json({ status: 'success' })
+    } catch (error) {
+      res.send(error)
+    }
   }
   static async checkLogin () {
     try {
